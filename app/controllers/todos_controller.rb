e5a5cb96c28e3fb2b9cf8e6bc347aeb5544a2bf2
David Lineman <dline215@gmail.com>
2021-02-17 16:18:20 -0500

    finished todos controller and RESTful routes

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -14,4 +14,23 @@ class TodosController < ApplicationController
   end
 
   # GET /todos/:id
+  def show
+    json_response(@todo)
+  end
+
+  # PUT /todos/:id
+  def update
+    @todo.update(@todo_params)
+    head :no_content
+  end
+
+  # DELETE /todos/:id
+  def destroy
+    # whitelist params
+    params.permit(:title, :created_by)
+  end
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
 end
