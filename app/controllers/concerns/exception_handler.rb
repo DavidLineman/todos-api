bd127168497184d87c8619a17c4f0473e9cafb14
DavidLineman <dline215@gmail.com>
2021-08-24 11:22:59 -0400

    defined custom Standard Error sub-classes to help handle exceptions raised. By defining error classes as sub-classes of standard error, you are able to rescue_from them once raised

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -2,6 +2,24 @@ module ExceptionHandler
   # provides the more graceful `included` method
   extend ActiveSupport::Concern
 
+  # Define custom error subclasses - rescue catches `StandardErrors`
+
+  class AuthenticationError < StandardError; end 
+  class MissingToken < StandardError; end 
+  class InvalidToken < StandardError; end 
+
+  included do
+    # Define custom handlers 
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      json_response({ message: e.message }, :not_found)
+    end
+  end 
+
   included do
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
@@ -12,6 +30,18 @@ module ExceptionHandler
     end
   end
 
-  
+  private
+
+  # JSON response with message; Status code 422 - unprocessable entity
+  def four_twenty_two(e)
+    json_response({ message: e.message }, :unprocessable_entity)
+  end
+
+  # JSON response with message: Status coe 401 - Unauthorized 
+  def unauthorized_request(e)
+    json_response({ message: e.message })
+  end
+
+
 end 
 
