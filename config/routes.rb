e4ef41c8204ac3b4901d2fc6670911e8194e7276
DavidLineman <dline215@gmail.com>
2021-08-25 10:34:44 -0400

    fixed typo in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :todos do
     resources :items 
   end
-  pot 'auth/login', to: 'authentication#authenticate'
+  post 'auth/login', to: 'authentication#authenticate'
 end
